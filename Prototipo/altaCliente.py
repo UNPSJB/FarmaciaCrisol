ff9115cc1245d7b28d6eb603a51907d489605afa
Eloy Williams <eloyw444@gmail.com>
2014-10-03 16:31:14 -0300

    Login Qdialog

--- Prototipo/altaCliente.py
+++ Prototipo/altaCliente.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'altaCliente.ui'
 #
-# Created: Fri Sep 26 11:13:13 2014
+# Created: Wed Oct  1 13:55:05 2014
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
--- Prototipo/modificarPresentacion.py
+++ Prototipo/modificarPresentacion.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'modificarPresentacion.ui'
 #
-# Created: Fri Sep 26 11:13:15 2014
+# Created: Wed Oct  1 13:55:07 2014
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
